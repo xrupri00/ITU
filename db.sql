954e5431cc185ec770c46a14a983714cb7dd4a36
2rec <jakubturcovsky@gmail.com>
2013-11-12 11:39:13 +0100

    update db

--- db.sql
+++ db.sql
@@ -1,13 +1,33 @@
 users:
-PK id
-email
-password
-name
-FK info
+int PK id
+varchar email
+varchar password
+varchar name
+int FK info
 
 info:
-PK id
+int PK id
+varchar name
+varchar street
+int(5) psc
+varchar city
+int ico
+varchar dic
+varchar registrace
+varchar ucet (nebo int?) -- zjistit, jestli musi byt na fakture banka vypsana i slovne
+int tel
+int fax null
+varchar internet null -- WTF?
 
 templates:
 PK id
 FK user_id
+// odberatel || prijemce
+varchar name
+varchar street
+int(5) psc
+varchar city
+int ico null
+varchar dic null
+varchar konst_symbol
+varchar cislo_obj
